01fb46822d3518d3da0b576c6a25e8cf7d47773f
micah_z <micah.a.zahn@gmail.com>
2022-05-18 15:29:40 -0500

    add all post routes

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our database, pass in your MySQL infomation for username and password
-const sequelize = new Sequelize('just_tech_news_db', 'root', 'root', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
